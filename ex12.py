520475a61b0e7f401711f2369936b553845b5edb
Your Name <you@example.com>
2018-10-04 17:38:36 +0530

    ex 11-17

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("What is your weight?")
+
+print "So, you are %r old, %r tall, and %r heavy." %(age, height, weight)
---
+++ ex14.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+script, user_name, title = argv
+prompt = '>>> '
+
+print "Hi %s, I am the %s script. we are doing %s title." % (user_name, script, title)
+print "I'd like to ask you a few questions."
+print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+""" % (likes, lives, computer)
